5a0d99ba394983407c1a75ffe7f32d9888a3066f
Matt Dawson <matthewtdawson@gmail.com>
2022-05-11 18:34:43 -0400

    Make getRollOutcome tests more terse

--- test/FitDRoller/getRollOutcome.js
+++ test/FitDRoller/getRollOutcome.js
@@ -5,53 +5,63 @@ const { FitDRoller } = require("../../fitd-roller.js");
 const fitdRoller = new FitDRoller();
 
 test("correctly handles multi-die critical success", t => {
-  const rolls = [{result: 6}, {result: 2}, {result: 6}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'critical-success');
+  const roll = [{result: 6}, {result: 2}, {result: 6}];
+  t.is(fitdRoller.getRollOutcome(roll), 'critical-success');
 });
 
 test("does not grant critical success in zeroMode", t=> {
-  const rolls = [{result: 6}, {result: 6}];
-  t.is(fitdRoller.getRollOutcome(rolls, true), 'success');
+  const roll = [{result: 6}, {result: 6}];
+  t.is(fitdRoller.getRollOutcome(roll, true), 'success');
 });
 
 test("correctly handles single die success", t => {
-  const rolls = [{result: 6}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'success');
+  const roll = [{result: 6}];
+  t.is(fitdRoller.getRollOutcome(roll), 'success');
 });
 
 test("correctly handles single die partial success", t => {
-  let rolls = [{result: 4}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'partial-success');
-  rolls = [{result: 5}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'partial-success');
+  const rolls = [
+    [{result: 4}],
+    [{result: 5}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll), 'partial-success');
+  });
 });
 
 test("correctly handles single die failure", t => {
-  let rolls = [{result: 1}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
-  rolls = [{result: 2}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
-  rolls = [{result: 3}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
+  const rolls = [
+    [{result: 1}],
+    [{result: 2}],
+    [{result: 3}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll), 'failure');
+  });
 });
 
 test("correctly handles multi-die success", t => {
-  const rolls = [{result: 1}, {result: 6}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'success');
+  const roll = [{result: 1}, {result: 6}];
+  t.is(fitdRoller.getRollOutcome(roll), 'success');
 });
 
 test("correctly handles multi-die partial success", t => {
-  let rolls = [{result: 1}, {result: 4}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'partial-success');
-  rolls = [{result: 1}, {result: 5}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'partial-success');
+  const rolls = [
+    [{result: 1}, {result: 4}],
+    [{result: 1}, {result: 5}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll), 'partial-success');
+  });
 });
 
 test("correctly handles multi-die failure", t => {
-  let rolls = [{result: 1}, {result: 3}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
-  rolls = [{result: 1}, {result: 2}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
-  rolls = [{result: 1}, {result: 1}];
-  t.is(fitdRoller.getRollOutcome(rolls), 'failure');
+  const rolls = [
+    [{result: 1}, {result: 3}],
+    [{result: 1}, {result: 2}],
+    [{result: 1}, {result: 1}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll), 'failure');
+  });
 });
