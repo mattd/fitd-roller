886b69cb175f1c90793c9892d6aea7c9b1b3114c
Matt Dawson <matthewtdawson@gmail.com>
2022-05-13 23:29:09 -0400

    Add zero die tests

--- test/FitDRoller/getRollOutcome.js
+++ test/FitDRoller/getRollOutcome.js
@@ -9,9 +9,30 @@ test("correctly handles multi-die critical success", t => {
   t.is(fitdRoller.getRollOutcome(roll), 'critical-success');
 });
 
-test("does not grant critical success in zeroMode", t=> {
+test("correctly handles zero die success", t=> {
   const roll = [{result: 6}, {result: 6}];
   t.is(fitdRoller.getRollOutcome(roll, true), 'success');
+});
+
+test("correctly handles zero die partial success", t => {
+  const rolls = [
+    [{result: 6}, {result: 5}],
+    [{result: 6}, {result: 4}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll, true), 'partial-success');
+  });
+});
+
+test("correctly handles zero die failure", t => {
+  const rolls = [
+    [{result: 6}, {result: 3}],
+    [{result: 6}, {result: 2}],
+    [{result: 6}, {result: 1}]
+  ];
+  rolls.forEach(roll => {
+    t.is(fitdRoller.getRollOutcome(roll, true), 'failure');
+  });
 });
 
 test("correctly handles single die success", t => {
